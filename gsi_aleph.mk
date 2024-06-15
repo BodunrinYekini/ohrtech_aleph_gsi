5c98592ad52c094396ab54720b01f1d57c5c3349
Bodunrin <yekinibodunrin@gmail.com>
2024-06-15 18:27:11 +0100

    copy

--- gsi_aleph.mk
+++ gsi_aleph.mk
@@ -50,7 +50,7 @@ $(call inherit-product, device/generic/common/gsi_product.mk)
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_release.mk)
 
-PRODUCT_PACKAGES += \
+PRODUCT_COPY_FILES += \
 $(call find-copy-subdir-files,*,$(DEVICE_PATH)/packages/product/overlay,$(TARGET_COPY_OUT_PRODUCT)/overlay)
 
 #product overlay
